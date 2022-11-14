c39bad226944417783b4c3753dbeecdced80785e
WeToTheMoon <97853964+WeToTheMoon@users.noreply.github.com>
2022-11-14 13:58:49 -0500

    adding data

--- code/final.py
+++ code/final.py
@@ -4,15 +4,32 @@ from keras import Sequential
 from keras.layers import LSTM, Dense
 import pandas as pd
 from matplotlib import pyplot as plt
-from utils import get_data, create_model
+from utils import format_ppm_x, create_model
 from keras.losses import MeanSquaredError
 from keras.metrics import MeanAbsoluteError
-from tensorflow.keras.optimizers import SGD
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
 
-X_train, X_test, Y_train, Y_test = get_data('/Users/zs/MathModeling/data/ppm.csv')
+NUM_PREV_VALS = 7
+
+data = pd.read_csv(r'/Users/zs/MathModeling/data/filtered_co2.csv')
+data = format_ppm_x(data, NUM_PREV_VALS)
+
+X = data.iloc[:, data.columns != 0]
+Y = data[0]
+
+x_train, x_test, y_train, y_test = train_test_split(X,Y, test_size=0.1)
 
 model = create_model()
 
-model.compile(optimizer=SGD(lr = 0.001, momentum=0.3, nesterov=True), loss=MeanSquaredError(), metrics=MeanAbsoluteError())
+optim = tf.keras.optimizers.Adagrad(learning_rate=0.01,
+    initial_accumulator_value=0.12)
 
-history = model.fit(X_train, Y_train, validation_data=(X_test, Y_test),verbose=1, epochs=100, batch_size=5)
+model.compile(optim, loss = 'mae', metrics = 'mse')
+
+history = model.fit(
+    x=x_train,
+    y=y_train,
+    validation_data=(x_test, y_test),
+    batch_size=16,
+    epochs=300)
